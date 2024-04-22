e57de6cf9926aeacf678f124eed228ce0d317c21
TOSHIBA <TOSHIBA@192.168.1.101>
2024-04-22 13:21:27 +0100

    console color change

--- src/main/java/cloudsyCreatesAffaire/CustomTestListener.java
+++ src/main/java/cloudsyCreatesAffaire/CustomTestListener.java
@@ -7,26 +7,27 @@ public class CustomTestListener extends TestListenerAdapter {
 
     @Override
     public void onTestSuccess(ITestResult tr) {
-        printTestResult(tr.getMethod().getMethodName(), 1, 0, 0);
+        printTestResult(tr.getMethod().getMethodName(), "PASS", 0, 0, "\u001B[32m"); // Green color
     }
 
     @Override
     public void onTestFailure(ITestResult tr) {
-        printTestResult(tr.getMethod().getMethodName(), 0, 1, 0);
+        printTestResult(tr.getMethod().getMethodName(), "FAIL", 0, 0, "\u001B[31m"); // Red color
     }
 
     @Override
     public void onTestSkipped(ITestResult tr) {
-        printTestResult(tr.getMethod().getMethodName(), 0, 0, 1);
+        printTestResult(tr.getMethod().getMethodName(), "SKIP", 0, 0, "\u001B[33m"); // Yellow color (skipped)
     }
 
-    private void printTestResult(String testName, int passes, int failures, int skips) {
+    private void printTestResult(String testName, String status, int passes, int failures, String color) {
         System.out.println("\n");
+        System.out.print(color);
         System.out.println("===============================================");
-        System.out.println(testName);
-        System.out.println("Total tests run: 1, Passes: " + passes + ", Failures: " + failures + ", Skips: " + skips);
+        System.out.println(testName + " - " + status);
+        System.out.println("Total tests run: 1, Passes: " + passes + ", Failures: " + failures);
         System.out.println("===============================================");
+        System.out.print("\u001B[0m");
         System.out.println("\n");
-
     }
 }
